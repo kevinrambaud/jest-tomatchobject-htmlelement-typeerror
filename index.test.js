ac674a9d1a80150a9d520bc841ceb33c7b958c9b
Kevin Rambaud <hello@kevinrambaud.com>
2023-06-09 09:50:06 +0200

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-test("toMatchObject throw TypeError: undefined is not a function when expected contains an value HTMLElement", () => {
+test("toMatchObject throw TypeError: undefined is not a function when expected contains a value HTMLElement", () => {
   const actual = { value: document.createElement("div") };
   const expected = { value: document.createElement("div") };
   expect(actual).toMatchObject(expected);
